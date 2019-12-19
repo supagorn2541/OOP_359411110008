a97d31048e00e337b53b3729a2c2a7422196a349
Supagorn <supagorn.tong2541@gmail.com>
2019-12-19 10:17:33 +0700

    Lap3: if,if-else

---
+++ src/Lab3/Control1.java
@@ -0,0 +1,29 @@
+package Lab3;
+
+public class Control1 {
+    public static void main(String[] args) {
+        //if
+        //     if(เงื่อนไข){
+        //     คำสั่งการทำงาน;
+        //     คำสั่งการทำงาน;
+
+
+        int x = 10;
+        if (x > 5)
+            System.out.println("x more than 5.");
+        if (x > 20){//F
+            System.out.println("x less than 20.");
+        System.out.println("Good Bye.");
+        }
+        if(x*10 == 100){//T
+            System.out.println("Yes,10*10=100.");
+        }
+
+}//main
+
+
+
+
+
+
+}//main
---
+++ src/Lab3/Control3.java
@@ -0,0 +1,45 @@
+package Lab3;
+
+import java.util.Scanner;
+
+public class Control3 {
+    public static void main(String[] args) {
+        //if-else-if : ทดสอบหลายเงื่อนไข ทำงานอย่างใดอย่างหนึ่งที่ผลการทดสอบเป็นจริง
+        //โครงสร้าง if-else-if
+
+//        if (เงื่อนไข 1)}
+//            คำสั่งเมื่อเงื่อนไขที่ 1เป็นจริง;
+//        }else if (เงื่อนไข 2){
+//            คำสีั่งเมื่อเงื่อนไขที่ 2เป็นจริง;
+//        }else if (เงื่อนไขที่ 3){
+//            คำสั่งเมื่อเงื่อนไขที่ 3เป็นจริง;
+//        }else if (เงื่อนไขที่ n){
+//            คำสั่งเมื่อเงื่อนไข nเป็นจริง;
+//        }else{
+//            คำสั่งที่ทำงานเมื่อทุกเงื่อนไขเป็นเท็จทั้งหมด;
+//        }
+
+        //How old are? >50 ==old, >20 ==teenager, <20 == young
+
+        Scanner sc=new Scanner(System.in);
+        System.out.print("How old are?:");
+        int age =sc.nextInt();
+        //test condition
+        if (age>50) {
+            System.out.println("You are old.");
+        }else if (age>20) {
+            System.out.println("You are teenager.");
+        }else {
+            System.out.println("You are young.");
+        }
+
+
+
+
+
+    }
+}
+
+
+
+
